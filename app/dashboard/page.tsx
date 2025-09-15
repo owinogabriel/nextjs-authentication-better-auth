a06bc8de04501dee448fa10795563b12a78bbbc4
owinogabriel <g73346920@gmail.com>
2025-09-15 17:32:58 +0300

    feat:when  session is not true redirect to the auth page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import { auth } from "@/lib/auth";
 import DashboardClientPage from "./dashboard-client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/auth");
+  }
   return <DashboardClientPage />;
 }
